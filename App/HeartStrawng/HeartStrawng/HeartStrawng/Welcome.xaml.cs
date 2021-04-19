d4e8359440e636a5d29b7eeaadc0ae59a40c5d3d
BRADY SMITH <u0903295@umail.utah.edu>
2021-04-19 11:02:48 -0600

    Add initial app template

---
+++ App/HeartStrawng/HeartStrawng/HeartStrawng/Welcome.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HeartStrawng
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Welcome : ContentPage
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+        async private void mainPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
